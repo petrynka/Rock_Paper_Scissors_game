84224a373c7e24f3e0e22296ea4153f75c137551
petrynka <61549290+petrynka@users.noreply.github.com>
2025-01-18 21:56:05 +0200

    Created function getComputerChoice
    Created function which return to us one of RockPaperScissors options

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+function getComputerChoice(){
+    let random = Math.floor(Math.random()*3+1);
+    let compChoice
+    if (random == 1) {
+        return compChoice = "Rock"
+    } else if(random == 2){
+        return compChoice = "Paper"
+    } else {
+        return compChoice = "Scissors"
+    }
+}
+
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
