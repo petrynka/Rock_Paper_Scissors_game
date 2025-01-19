bedf29abb1d50c7d22862833c440aa5c28b9a882
petrynka <61549290+petrynka@users.noreply.github.com>
2025-01-19 20:12:52 +0200

    Complete whole game
    Created new function playGame with logic for whole game

--- script.js
+++ script.js
@@ -12,46 +12,67 @@ function getComputerChoice(){
     }
 }
 
-let humanChoice;
+// let humanChoice;
 
 function getHumanChoice(){
-    humanChoice = prompt("Please write your Rock, Paper or Scissors choice", "");
-    return humanChoice;
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice){
-    let hC = humanChoice.toLowerCase();
-    let cC = computerChoice.toLowerCase();
-
-    if(hC == "rock" || hC == "paper" || hC == "scissors"){
-        if(hC=="rock"&&cC=="scissors"||
-            hC=="paper"&&cC=="rock"||
-            hC=="scissors"&&cC=="paper"){
-             humanScore++
-             console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-             console.log(`Your score: ${humanScore}`);
-             console.log(`Computer score ${computerScore}`);
-            } else if(hC=="rock"&&cC=="rock"||
-                      hC=="paper"&&cC=="paper"||
-                      hC=="scissors"&&cC=="scissors"){
-                         console.log("This time it's a draw, try again.")
-                      } else{
-                         computerScore++
-                         console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-                         console.log(`Your score: ${humanScore}`);
-                         console.log(`Computer score ${computerScore}`);
-                      }
-    }else{
-        console.log("You entered wrong value!")
-    }            
+   let humanChoiceInput = prompt("Please write your Rock, Paper or Scissors choice", "");
+    return humanChoiceInput;
 }
 
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
 
+
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    let count = 0;
+
+    function playRound(humanChoice, computerChoice){
+        let hC = humanChoice.toLowerCase();
+        let cC = computerChoice.toLowerCase();
+    
+        if(hC == "rock" || hC == "paper" || hC == "scissors"){
+            if(hC=="rock"&&cC=="scissors"||
+                hC=="paper"&&cC=="rock"||
+                hC=="scissors"&&cC=="paper"){
+                 humanScore++
+                 console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                 console.log(`Your score: ${humanScore}`);
+                 console.log(`Computer score ${computerScore}`);
+                } else if(hC=="rock"&&cC=="rock"||
+                          hC=="paper"&&cC=="paper"||
+                          hC=="scissors"&&cC=="scissors"){
+                             console.log("This time it's a draw, try again.")
+                          } else{
+                             computerScore++
+                             console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                             console.log(`Your score: ${humanScore}`);
+                             console.log(`Computer score ${computerScore}`);
+                          }
+        }else{
+            console.log("You entered wrong value!")
+        }            
+    }
+
+    while (count < 5) {
+        playRound(getHumanChoice(),getComputerChoice())
+        count++
+    }
+    if (humanScore>computerScore) {
+        console.log("Congratulation you win with score " + humanScore +":"+computerScore)
+    } else if(humanScore == computerScore) {
+        console.log("This time after five rounds you have a draw.")
+    } else{
+        console.log("Sorry but you lose with score " + computerScore +":"+humanScore)
+    }
+}
+
+// playGame();
+
+playGame();
