1df3f870a5a0e5cedae340f3d3297725a83ecbf5
petrynka <61549290+petrynka@users.noreply.github.com>
2025-01-19 16:01:36 +0200

    Completed logic for game round
    Created 2 var for counting wins and create all logic for game round

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+
 
 function getComputerChoice(){
     let random = Math.floor(Math.random()*3+1);
@@ -19,4 +19,39 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-console.log("Rock" === "rock");
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    let hC = humanChoice.toLowerCase();
+    let cC = computerChoice.toLowerCase();
+
+    if(hC == "rock" || hC == "paper" || hC == "scissors"){
+        if(hC=="rock"&&cC=="scissors"||
+            hC=="paper"&&cC=="rock"||
+            hC=="scissors"&&cC=="paper"){
+             humanScore++
+             console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+             console.log(`Your score: ${humanScore}`);
+             console.log(`Computer score ${computerScore}`);
+            } else if(hC=="rock"&&cC=="rock"||
+                      hC=="paper"&&cC=="paper"||
+                      hC=="scissors"&&cC=="scissors"){
+                         console.log("This time it's a draw, try again.")
+                      } else{
+                         computerScore++
+                         console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                         console.log(`Your score: ${humanScore}`);
+                         console.log(`Computer score ${computerScore}`);
+                      }
+    }else{
+        console.log("You entered wrong value!")
+    }            
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
